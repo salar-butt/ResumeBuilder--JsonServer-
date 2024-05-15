e5aa91daf1e35911ff6e692856684d691c28327c
unknown <salarbutt321@gmail.com>
2024-05-15 18:49:25 +0500

    resume changes

--- src/Actions/Action.js
+++ src/Actions/Action.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const baseurl="https://json-api-xrl5.onrender.com"
+const baseUrl="https://json-api-xrl5.onrender.com"
 
 export const postjobseekar = (object) => async(dispatch) => {
     try {
-        const {data} = await axios.post("http://localhost:8000/jobseekar",object)
+        const {data} = await axios.post(`${baseUrl}/resume`,object)
         dispatch({ type: "JOBSEEKAR_ADD_SUCCES", payload: data });
     }
     catch (error) {
@@ -16,7 +16,7 @@ export const loginJobseekar = (email, password ) => async (dispatch) => {
 
     try {
         dispatch ({ type: "LOGIN_JOBSEEKAR_REQUEST"})
-      const {data} = await axios.get("http://localhost:8000/jobseekar");
+      const {data} = await axios.get(`${baseUrl}/resume`);
 
       const user = data.find((user) => user.email === email && user.password === password);
 
@@ -36,7 +36,7 @@ export const loginJobseekar = (email, password ) => async (dispatch) => {
   export const getSinglejobseekar = (id) => async(dispatch) => {
     try {
         dispatch ({ type: "SINGLE_JOBSEEKAR_REQUEST"});
-        const {data} = await axios.get(`http://localhost:8000/jobseekar/${id}`);
+        const {data} = await axios.get(`${baseUrl}/resume/${id}`);
         dispatch ({ type: "SINGLE_JOBSEEKAR_SUCCES", payload: data})
     } catch (error) {
         dispatch ({ type: "SINGLE_JOBSEEKAR_FAIL", payload: error.message})
@@ -45,7 +45,7 @@ export const loginJobseekar = (email, password ) => async (dispatch) => {
 
   export const addProfileInfo = (id, Object) => async (dispatch) => {
     try {
-        const {data} = await axios.patch(`http://localhost:8000/jobseekar/${id}`, Object)
+        const {data} = await axios.patch(`${baseUrl}/resume/${id}`, Object)
         console.log(data,"yo bro data");
         dispatch({ type: "UPDATE_PROFILE_SUCCES", payload: data})
     } catch(error){
@@ -53,20 +53,20 @@ export const loginJobseekar = (email, password ) => async (dispatch) => {
     }
 }
 
-// export const addContactInfo = (id, Object) => async (dispatch) => {
-//     try {
-//         const {data} = await axios.put(`http://localhost:8000/jobseekar/${id}`, Object)
-//         dispatch({ type: "ADD_CONTACTINFO_SUCCES", payload: data})
-//     } catch(error){
-//         dispatch({ type: "ADD_CONTACTINFO_FAILURE", payload: error.message})
-//     }
-// }
+export const addContactInfo = (id, Object) => async (dispatch) => {
+    try {
+        const {data} = await axios.put(`${baseUrl}/resume/${id}`, Object)
+        dispatch({ type: "ADD_CONTACTINFO_SUCCES", payload: data})
+    } catch(error){
+        dispatch({ type: "ADD_CONTACTINFO_FAILURE", payload: error.message})
+    }
+}
 
-// export const addExperienceInfo = (id, Object) => async (dispatch) => {
-//     try {
-//         const {data} = await axios.put(`http://localhost:8000/jobseekar/${id}`, Object)
-//         dispatch({ type: "ADD_EXPERIENCEINFO_SUCCES", payload: data})
-//     } catch(error){
-//         dispatch({ type: "ADD_EXPERIENCEINFO_FAILURE", payload: error.message})
-//     }
-// }
+export const addExperienceInfo = (id, Object) => async (dispatch) => {
+    try {
+        const {data} = await axios.put(`${baseUrl}/resume/${id}`, Object)
+        dispatch({ type: "ADD_EXPERIENCEINFO_SUCCES", payload: data})
+    } catch(error){
+        dispatch({ type: "ADD_EXPERIENCEINFO_FAILURE", payload: error.message})
+    }
+}
